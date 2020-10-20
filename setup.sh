25b547ce3374c28bb5d4001475678768fce5badd
guoxinggang <gxg@bupt.edu.cn>
2020-10-20 17:28:47 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,25 +1,8 @@
 mkdir -p ~/.streamlit/
 
-
-
-echo "\
-
-[general]\n\
-
-email = \"gxg@bupt.edu.cn\"\n\
-
-" > ~/.streamlit/credentials.toml
-
-
-
-echo "\
-
-[server]\n\
-
-headless = true\n\
-
-enableCORS=false\n\
-
-port = $PORT\n\
-
+echo "
+[server]
+headless=true
+enableCORS=false
+port=$PORT
 " > ~/.streamlit/config.toml
